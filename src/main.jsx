34cbc775f74c3a33b05393e572a9d8134f5b3324
NeoHBz <neohbz@gmail.com>
2025-09-04 05:02:45 +0530

    added: tailwindcss v3

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
+    <footer className="fixed bottom-2 w-full text-center">
+      <a
+        href="https://neohbz.com"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-gray-400 hover:text-gray-800 transition"
+      >
+        with ❤️ by neohbz
+      </a>
+    </footer>
   </StrictMode>,
-)
+);
